78ef85dfa414e80e0b68175dfeef4e3113029d5b
Ikutaro Hamada <ikutaro.hamada@gmail.com>
2023-11-23 14:32:53 +0900

    Update

--- conf.py
+++ conf.py
@@ -45,9 +45,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = ['_themes', ]
 
-# html_theme_path = ['_themes', ]
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_logo = './img/state_logo_red.png'
 
