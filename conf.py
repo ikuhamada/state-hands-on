d156ff29e21e3fae7141ec9a5fe1c466f23b8785
Ikutaro Hamada <ikutaro.hamada@gmail.com>
2025-09-01 13:45:05 +0900

    Add a new image

--- conf.py
+++ conf.py
@@ -49,7 +49,7 @@ html_theme_path = ['_themes', ]
 
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-html_logo = './img/state_logo_red.png'
+html_logo = './img/state_logo_red_blue.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
